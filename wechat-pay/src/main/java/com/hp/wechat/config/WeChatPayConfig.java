5435d221d20db4f4d6a5ac2f50c33bd9ddf7dd27
胡鹏 <hup@lubanjianye.com>
2022-03-31 17:05:50 +0800

    basic WeChat API

---
+++ wechat-pay/src/main/java/com/hp/wechat/config/WeChatPayConfig.java
@@ -0,0 +1,37 @@
+package com.hp.wechat.config;
+
+
+import java.io.InputStream;
+
+/**
+ * 微信支付配置
+ *
+ * @Author: HP
+ */
+public interface WeChatPayConfig {
+    String getAppId();
+
+    String getMchId();
+
+    String getKey();
+
+    default InputStream getCertStream() {
+        return null;
+    }
+
+    ;
+
+    default boolean getUseSandbox() {
+        return false;
+    }
+
+    default int getHttpConnectTimeoutMs() {
+        return 6000;
+    }
+
+    ;
+
+    default int getHttpReadTimeoutMs() {
+        return 6000;
+    }
+}
